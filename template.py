8df4c02fa58587c2bfc8d370314dc55284d4212b
Aditya Soni <adityasoni5859@gmail.com>
2023-01-30 23:49:03 +0530

    initialized dvc

--- template.py
+++ template.py
@@ -1,16 +1,17 @@
 import os
 
 dirs = [
-os.path.join('data','raw'),
-os.path.join('data','processed'),
-'notebooks',
-'saved_models',
-'src'
+    os.path.join('data', 'raw'),
+    os.path.join('data', 'processed'),
+    os.path.join('data', 'data_given'),
+    'notebooks',
+    'saved_models',
+    'src'
 ]
 
 for dir_ in dirs:
     os.makedirs(dir_, exist_ok=True)
-    with open(os.path.join(dir_, ".gitkeep"),"w") as f:
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f:
         pass
 
 
@@ -18,7 +19,7 @@ files = [
     "dvc.yaml",
     "params.yaml",
     ".gitignore",
-    os.path.join("src","__init__.py")
+    os.path.join("src", "__init__.py")
 ]
 
 for file_ in files:
